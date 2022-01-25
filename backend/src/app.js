203d2e6b8bc03b5ca136e65ba856710268d47c9b
Kanwar007 <vijayblp.kanwar@gmail.com>
2022-01-26 02:20:38 +0800

    --- Auto-Git Commit ---

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,14 @@
 const { request, response } = require('express');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+mongoose.connect('mongodb+srv://admin:admin@cluster0.xk5z0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', )
+.then(()=>{console.log('connected data base')})
+.catch(error=()=>{
+    console.log('error');
+})
 app.get('/',(request,response)=>{
     response.send('back end call')
 })
@@ -8,3 +16,14 @@ app.listen(4000,()=>{
     console.log('server is running');
 })
 
+const schema=buildSchema(`
+  type Query {
+      name : string
+  }
+`)
+const root = { name: () => 'Pushpa' };
+app.use('/graphql',graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql:true
+}))
