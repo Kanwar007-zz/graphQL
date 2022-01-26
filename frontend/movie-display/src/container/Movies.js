8896ec9a5e32f6fbd2c7c0e6d843af21d5e1b27e
Kanwar007 <vijayblp.kanwar@gmail.com>
2022-01-26 23:44:28 +0800

    --- Auto-Git Commit ---

--- frontend/movie-display/src/container/Movies.js
+++ frontend/movie-display/src/container/Movies.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Movie from './Movie';
 import { useQuery, gql } from "@apollo/client";
 
@@ -12,39 +12,24 @@ const allMovies = gql`
        }
    }
 `
-class Movies extends Component {
-    state={
-        movies:[
-            {
-          id:'1',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-           },
-           {
-            id:'2',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-             },
-             {
-                id:'3',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-                 },
-                 {
-                    id:'4',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-                     },
-                     {
-                        id:'5',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-                         },
-    ]
-    }
-    render() {
-        return (
-             <div className='movies'>
-             {this.state.movies.map((movie)=>{
-                 return <Movie key={movie.id}
-                 name={movie.name}
-                 type={movie.typeofmoive}
-                 year={movie.year}
-                 image={movie.image}/>
-             })} 
-            </div>
-        );
-    }
-}
 
-export default useQuery(allMovies)(Movies);
+function Movies() {
+    const { loading, error, data } = useQuery(allMovies);
+  
+    if (loading) return null;
+    if (error) return `Error! ${error}`;
+  
+    return (
+     
+      <div className='movies'>
+                {data.movies.map((movie)=>{
+                    return <Movie key={movie.id}
+                    name={movie.name}
+                    type={movie.typeofmoive}
+                    year={movie.year}
+                    image={movie.image}/>
+                })} 
+               </div>
+    );
+  }
+  export default Movies;
