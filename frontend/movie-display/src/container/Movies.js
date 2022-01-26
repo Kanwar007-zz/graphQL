6c4e86e458880ad83844810965316a1f27d3d9fe
Kanwar007 <vijayblp.kanwar@gmail.com>
2022-01-26 21:39:13 +0800

    --- Auto-Git Commit ---

--- frontend/movie-display/src/container/Movies.js
+++ frontend/movie-display/src/container/Movies.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
-import movie from './Movie';
 import Movie from './Movie';
+import {useQuery, gql } from "@apollo/client";
+
+
+const allMovies = gql`
+   {
+    movies{
+           name
+           type
+           year
+       }
+   }
+`
+
 
 class Movies extends Component {
-    state={
-        movies:[
-            {
-          id:'1',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-           },
-           {
-            id:'2',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-             },
-             {
-                id:'3',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-                 },
-                 {
-                    id:'4',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-                     },
-                     {
-                        id:'5',  name:'vijay', typeofmoive:'action',year:'1987',image:'https://rb.gy/mwzdxh'
-                         },
-    ]
+    movies = useQuery(allMovies);
     }
     render() {
         return (
             <div className='movies'>
-             {this.state.movies.map((movie)=>{
+             {data.map((movie)=>{
                  return <Movie key={movie.id}
                  name={movie.name}
                  type={movie.typeofmoive}
